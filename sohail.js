6c2c1b4989aa51067f042667ecf6aba06ce5bd8f
sohailkazi12 <ksohailkazi123@gmail.com>
2025-01-08 16:03:47 +0530

    Add files via upload

---
+++ sohail.js
@@ -0,0 +1,8 @@
+let btn=document.createElement("button");
+btn.innerText="click me";
+btn.style.color="#578E7E";
+btn.style.backgroundColor="#3D3D3D";
+console.log("btn");
+document.querySelector("body").append(btn);
+
+document.body.style.backgroundColor="#F0BB78";
